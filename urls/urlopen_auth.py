6428d1a9e4f8e99fa7f859c7783220dfb3cac998
schampoux <sergiochampoux@gmail.com>
2025-05-29 17:22:33 -0400

    supports both handler and request approaches

--- urls/urlopen_auth.py
+++ urls/urlopen_auth.py
@@ -1,5 +1,8 @@
 #!/home/spunion/.virtualenvs/sockets/bin/python
 
+import urllib.request, urllib.error, urllib.parse 
+from base64 import encodebytes
+
 # Two ways to handle basic authentication. 
     # 1: handler, opener, install opener. 
     # 2: HTTP client request with auth headers. 
@@ -8,7 +11,6 @@
         # > sudo python3 -m http.server 80
         # default port is 80, we don't specify this below in URL
 
-import urllib.request, urllib.error, urllib.parse 
 
 LOGIN = 'test_user'
 PASSWD = 'password'
@@ -20,12 +22,20 @@ def handler_version(url):
     hdlr.add_password(REALM, urllib.parse.urlparse(url)[1], LOGIN, PASSWD)
     opener = urllib.request.build_opener(hdlr)
     urllib.request.install_opener(opener)
-    print(url)
     return(url) 
 
+def request_version(url):
+    request = urllib.request.Request(url) 
+    b64str = encodebytes(
+        bytes('%s:%s' % (LOGIN, PASSWD), 'utf-8'))[:-1]
+    request.add_header("Authorization", "Basic %s" % b64str)
+    return request
+
 if __name__ == "__main__":
-    url = handler_version(URL)
-    f=urllib.request.urlopen(url)
-    print(str(f.readline(), 'utf-8'))
-    f.close()
+    for approach in ('handler', 'request'):
+        print("********Using %s approach" % approach)
+        url = eval('%s_version' % approach)(URL)
+        f=urllib.request.urlopen(url)
+        print(str(f.readline(), 'utf-8'))
+        f.close()
 
